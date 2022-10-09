87930a7de58ce2f91429f625c8149f29c1b4196d
Gunter <gw6sk@arcor.de>
2022-10-09 18:35:01 +0200

    einfacher Sensortest im main.cpp eingebunden

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <Arduino.h>
 #include <DueTimer.h>
 //#include <HBStringTest.h>
-#include <HallSensor.h>
+//#include <HallSensor.h>
 #include <constants.h>
+#include <testsensor.h>
 
 // Variables need to be global to get into setup and loop
-HallSensor hall(HALLPIN, DISTANCESENSORMAGNET, BALLDIAMETER);
+//HallSensor hall(HALLPIN, DISTANCESENSORMAGNET, BALLDIAMETER);
 
 void setup() {
   // put your setup code here, to run once:
   // hbStringSetup();
-  Serial.begin(9600);
-    while(!Serial.available()) {} //warten, dass der serielle Monitor online ist
-    Serial.readString();
-    Serial.println("Jetzt geht es los.");
-    hall.SetRawValue(2077); //2077
-    hall.DispAllAtSerial(); 
+  testSensorSetup();
 }
 
 void loop() { 
   // put your main code here, to run repeatedly:
   // hbStringLoop();
+  testSensorLoop();
 }
